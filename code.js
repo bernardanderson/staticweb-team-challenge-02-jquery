0053a31b96ce9b03fe08a9df66bfa9234b4178e0
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-17 15:27:13 -0600

    Completed the jQuery version of the  staticweb team challenge 02

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   
 // Builds the three divs on the main page
-  $(document.body).add("<div id='1'></div>").add("<div id='2'></div>").add("<div id='3'></div>").prependTo(document.body);
+  $(document.body).add("<div></div>").add("<div></div>").add("<div></div>").prependTo(document.body);
 
-// Initial styling of the three divs on the main page
-  $("div").css( "display", "inline-block").css( "width", "200px").css( "height", "200px").css( "margin", "2px").css( "background-color", "lightgray");
+// Beautification of the divs on the main page
+  $("div").css("display", "inline-block").css("width", "200px").css("height", "200px").css("margin", "2px").css("background-color", "lightgray");
 
-// This captures any clicks on any div and gives it a red border
+// This captures any clicks on any div and activates the coloring/styling of the divs, addition of the <input> &
+//  <button> and the coloring of divs from the text input
   $("div").click(function() {
 
-    // This check to see if a border has already been clicked, if so then
-    //  then it removes/toggles off the border.  Otherwise, it clears all borders
-    //  and then adds the border to the new div.
+    // This checks to see if a border has already been clicked, if so it removes/toggles off the border.
+    //  Otherwise, it clears all borders and then adds the border to the selected div.
     if ($(this).css("border") === "2px solid rgb(255, 0, 0)") {
       $(this).css("border", "").removeClass("border");
     } else {
@@ -19,19 +19,20 @@ $(document).ready(function() {
       $(this).css("border", "2px solid rgb(255, 0, 0)").addClass("border");
     };
 
+    // This checks to see if there is an text <input> on the DOM, if not it adds both the <input> and
+    //  the activation <button>
     if (!$("input").length) {
-      $(document.body).append("<input id='text'></input>").append("<button id='button'>Change Color</button>");
+      $(document.body).append("<input></input>").append("<button>Change Color</button>");
       $("input").css("display", "block").attr("placeholder", "Enter a color");
-    }
+    };
+    
+    // This checks to see if the button is clicked. If so, it changes the bgcolor of the clicked div
+    //  to what is in text input. 
+    $("button").click(function() {
+      var color = $("input").val();
+      $(".border").css("background-color", color);
+    });
   });
-
-  $("button").click(function() {
-    var color = $("input").val();
-    console.log("Color: ", color);
-    $(".border").css("background-color", color);
-  });
-
-
 });
 
 
